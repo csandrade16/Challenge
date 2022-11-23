70b5b06c77302e4bb7e37f392c4493f59842ba9e
csandrade16 <81444203+csandrade16@users.noreply.github.com>
2022-11-23 19:00:38 -0300

    Initial Commit

---
+++ Entities/Enum/Enum.cs
@@ -0,0 +1,15 @@
+﻿namespace Entities.Enum
+{
+    public class Enum
+    {
+        public enum EmployeeArea
+        {
+            BoardOfDirectors = 1,
+            Accountability = 2,
+            Technology = 2,
+            Finance = 2,
+            GeneralServices = 3,
+            CustomerRelations = 4
+        }
+    }
+}
